8aa85c879b0cd24af51018fbe420facb7a2dc838
chengwx <984533862@qq.com>
2017-12-05 17:26:11 +0800

    获取及展示用户信息

---
+++ vue-modle/src/components/Custorm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="custorm container">
+    <h1 class="page-header">用户管理系统</h1>
+    <table class="table table-striped">
+    	<thead>
+    		<tr>
+    			<th>姓名</th>
+    			<th>电话</th>
+    			<th>邮箱</th>
+    			<th></th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<tr  v-for="custorm in custorms">
+    			<td>{{custorm.name}}</td>
+    			<td>{{custorm.phone}}</td>
+    			<td>{{custorm.email}}</td>
+    			<td>{{custorm.age}}</td>
+    		</tr>
+    	</tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'custorm',
+  data () {
+    return {
+     custorms:[]
+    }
+  },
+  methods:{
+  	fetchCustomers(){
+  		this.$http.get("http://localhost:3000/users")
+  		.then(function(response){
+  			// console.log(response);
+  			this.custorms = response.body;
+  		})
+  	}
+  },
+  created(){
+  	 this.fetchCustomers();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
